932e64ccefbccaddb6ff85b2ef110e39cfd72a80
Jose-Tejero <j_jtejero@hotmail.com>
2022-01-25 22:46:56 -0600

    Estileando el modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,31 +1,37 @@
 import React from 'react';
 
 const Modal = ({deleteUser, setIsModalOpen, isSelectedId}) => {
+
+    const styles = {
+        position: "absolute",
+        width: "100%",
+        height: "100vh",
+        background: "rgba(0,0,0, 0.3)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        zIndex: 20
+    }
+
+    const subStyles = {
+        width: "50%",
+        height: "200px",
+        background: "rgba(255, 70, 70, 0.9)",
+        textAlign: "center",
+        borderRadius: "20px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-evenly"
+    }
+
     return (
-        <div
-          style={{
-            position: "absolute",
-            width: "100%",
-            height: "100vh",
-            background: "rgba(0,0,0, 0.3)",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            zIndex: 20,
-          }}
-        >
-          <div
-            style={{
-              width: "50%",
-              height: "50%",
-              background: "rgba(255, 70, 70, 0.9)",
-              textAlign: "center",
-              borderRadius: "20px"
-            }}
-          >
+        <div style={styles}>
+          <div style={subStyles}>
             <h2>¿Seguro desea eliminar este usuario?</h2>
-            <button onClick={() => {deleteUser(isSelectedId)}}>Aceptar</button>
-            <button onClick={() => setIsModalOpen(false)}>Cancelar</button>
+            <div className="modal-buttons">
+                <button onClick={() => deleteUser(isSelectedId)}>Aceptar</button>
+                <button onClick={() => setIsModalOpen(false)}>Cancelar</button>
+            </div>
           </div>
         </div>
     );
